6c306558934fffeb1e6abcea170712d7238429d6
Egor <alex.pankov.new@gmail.com>
2022-09-05 00:24:10 +0300

    '05-09-2022'

--- urls.py
+++ urls.py
@@ -7,9 +7,8 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('api/v1/rk/', NewApi.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/getrk', GetRK.as_view()),
     path('api/rktest', StoryTestApi.as_view()),
     path('api/rktest/2', StoryTestApi2.as_view()),
--- views.py
+++ views.py
@@ -9,30 +9,27 @@ from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-# Create your views here.
-# class NewApi(generics.ListCreateAPIView):
-#     queryset = SidePlanes.objects.all()
-#     serializer_class = SidePlanesSerializer
 
 class StoryTestApi(generics.ListCreateAPIView):
         queryset = RkCompany.objects.filter(rk_id=69)   #32
         serializer_class = RkCompanySerializer
 
-class GetRK(generics.ListCreateAPIView):
-        queryset = Rk.objects.filter(client_id=8)
+# class GetRK(generics.ListCreateAPIView):
+#         queryset = Rk.objects.filter(client_id=8)
+#         serializer_class = RkSerializer
+class GetRK(APIView):
         serializer_class = RkSerializer
+
+        def get(self,request, *args, **kwargs):
+                data = Rk.objects.filter(client__founder__id=request.user.id)
+                serializer = RkSerializer(data, many=True)
+                return Response(serializer.data)
+
 
 class StoryTestApi2(generics.ListCreateAPIView):
         queryset = Totalplanes.objects.filter(Razom_number=64218)
         serializer_class = TotalplanesSerializer
 
-# @api_view(['POST'])
-# def postTest(request):
-#         if request.method == 'POST':
-#                 rk = JSONParser().parse(request)[0]
-#                 rk_2 = list(rk.values())[0]
-#                 queryset = RkCompany.objects.filter(rk_id=rk_2)
-#                 serializer_class = RkSerializer
 
 class RkTestApi(APIView):
         serializer_class = RkCompanySerializer
